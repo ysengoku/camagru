aa03bb2bfee1ddc0dca15a17228fae3553533759
Yuko SENGOKU <ysengoku.tourisme@gmail.com>
2025-10-15 15:42:49 +0200

    feat: add email verification field to users table & getDB method to Model class

--- app/src/Core/Model.php
+++ app/src/Core/Model.php
@@ -1,6 +1,9 @@
 <?php
 
+require_once __DIR__.'/Database.php';
+
 abstract class Model {
+    protected static ?Database $db = null;
     protected static string $name;
     protected static array $schema = [];
 
@@ -11,5 +14,12 @@ abstract class Model {
             $columns[] = "`$field` $type";
         }
         return ("CREATE TABLE IF NOT EXISTS `$tableName` (" . implode(", ", $columns) . ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;");
+    }
+
+    protected static function getDb(): Database {
+        if (self::$db === null) {
+            self::$db = Database::getInstance();
+        }
+        return self::$db;
     }
 }
--- app/src/Models/User.php
+++ app/src/Models/User.php
@@ -9,6 +9,8 @@ class User extends Model {
         'username' => 'VARCHAR(255) UNIQUE NOT NULL',
         'email' => 'VARCHAR(255) UNIQUE NOT NULL',
         'password_hash' => 'VARCHAR(255) NOT NULL',
+        'email_varified' => 'TINYINT(1) DEFAULT 0 NOT NULL',
+        'verification_token' => 'VARCHAR(64) DEFAULT NULL',
         'created_at' => 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP',
     ];
 
@@ -16,6 +18,8 @@ class User extends Model {
     public string $username = '';
     public string $email = '';
     public string $password_hash = '';
+    public bool $email_varified = false;
+    public string $verification_token = '';
     public ?string $created_at = '';
 
     public function __construct() {
