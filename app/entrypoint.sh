1014056fde4f6fbfc94b953e4fe415a11cb0e7e7
ysengoku <ysengoku.tourisme@gmail.com>
2025-10-23 22:17:00 +0200

    feat: update Dockerfile and docker-compose to migrate to node.js

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 set -e
 
-mkdir -p /var/www/html/public/uploads
-chown -R www-data:www-data /var/www/html/public/uploads
-chmod 755 /var/www/html/public/uploads
+mkdir -p /usr/src/app/public/uploads
+chown -R /usr/src/app/public/uploads
+chmod 755 /usr/src/app/public/uploads
 
 until mysqladmin ping -h"$DB_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" --ssl=0 --silent; do
   echo "Waiting for database..."
   sleep 2
 done
-
-echo "Running database migrations..."
-php migrate.php
 
 exec "$@"
