c6888af5c4e08a176239a83776e3d5cb50c0c7ce
ysengoku <ysengoku.tourisme@gmail.com>
2025-10-30 20:13:58 +0100

    feature: add migration to database

--- database/init_db.sh
+++ database/init_db.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
 
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "USE \`${MYSQL_DATABASE}\`;"
+echo "Initializing database and applying migrations..."
+
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<EOSQL
+	USE \`${MYSQL_DATABASE}\`;
+	SOURCE /migrations/001_initial_schema.sql;
+EOSQL
+
+echo "Migration completed successfully."
