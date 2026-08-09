3eaa4e2343197880aa8a82aace71a7cd7f6478c8
ysengoku <ysengoku.tourisme@gmail.com>
2026-08-09 17:21:49 +0200

    minor: update no-posts view in feed and 404

--- app/src/Views/feed/index.php
+++ app/src/Views/feed/index.php
@@ -9,17 +9,15 @@ require_once __DIR__ . '/noPosts.php';
 
 ?>
 
-<div class="p-4 flex-1">
-    <?php if (empty($posts)) : ?>
-      <?php echo render_no_posts(); ?>
-    <?php else : ?>
-        <div id="feed-container" class="grid grid-cols-auto grid-auto-rows grid-gap-4">
+<?php if (empty($posts)) : ?>
+    <?php echo render_no_posts(); ?>
+<?php else : ?>
+    <div id="feed-container" class="grid grid-cols-auto grid-auto-rows grid-gap-4 p4">
         <?php foreach ($posts as $post) : ?>
             <?php echo render_post_preview($post); ?>
         <?php endforeach; ?>
-        </div>
-        <?php if ($count > count($posts)) : ?>
-            <div id="observer"></div>
-        <?php endif; ?>
+    </div>
+    <?php if ($count > count($posts)) : ?>
+        <div id="observer"></div>
     <?php endif; ?>
-</div>
+<?php endif; ?>
